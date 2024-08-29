1b33071d6c82f5ddf57ab7507b43c22a69200135
b43NnUNF4vidFYFhpqaLWy2ANawtRbMtUXZY9Pf <77827828+b43NnUNF4vidFYFhpqaLWy2ANawtRbMtUXZY9Pf@users.noreply.github.com>
2024-08-29 15:16:52 +0200

    feat: add spatial indexing

--- src/Program.cs
+++ src/Program.cs
@@ -8,7 +8,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDBContext>(options => {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        x => x.UseNetTopologySuite());
 });
 
 var app = builder.Build();
---
+++ src/SpatialIndexManager/HPRtreeManager.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.Index.HPRtree;
+
+namespace MarketTrustAPI.SpatialIndexManager
+{
+    public class HPRtreeManager : ISpatialIndexManager
+    {
+        private readonly HPRtree<ILocatable> _hprTree;
+
+        public HPRtreeManager(IEnumerable<ILocatable> items)
+        {
+            _hprTree = new HPRtree<ILocatable>();
+
+            foreach (ILocatable item in items)
+            {
+                _hprTree.Insert(item.GetLocation().EnvelopeInternal, item);
+            }
+        }
+
+        public IList<ILocatable> GetPointsInRadius(Point center, double radius)
+        {
+            Envelope box = new Envelope(
+                center.Coordinate.X - radius, center.Coordinate.X + radius,
+                center.Coordinate.Y - radius, center.Coordinate.Y + radius
+            );
+
+            List<ILocatable> nearby = _hprTree
+                .Query(box)
+                .Where(candidate => center.Distance(candidate.GetLocation()) <= radius)
+                .ToList();
+
+            return nearby;
+        }
+    }
+}
