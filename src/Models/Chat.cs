dfd50b0b4e0bbdcc4948294a6271c8456c3f27ec
b43NnUNF4vidFYFhpqaLWy2ANawtRbMtUXZY9Pf <77827828+b43NnUNF4vidFYFhpqaLWy2ANawtRbMtUXZY9Pf@users.noreply.github.com>
2025-01-27 20:06:02 +0100

    fix(models): add missing navigation to Messages in Chat

--- src/Models/Chat.cs
+++ src/Models/Chat.cs
@@ -9,5 +9,6 @@ namespace MarketTrustAPI.Models
     {
         public int Id { get; set; }
         public List<User> Participants { get; set; } = new List<User>();
+        public List<Message> Messages { get; set; } = new List<Message>();
     }
 }
